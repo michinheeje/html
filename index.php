97ce4baee71d4d1d6c121306735a2f9a5f341ec3
michinheeje <41563223+michinheeje@users.noreply.github.com>
2019-04-30 13:47:33 +0900

    Rename index.html to index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
 <head>
-<title>»çÄ¢ ¿¬»ê ¿¬½À</title>
+<title>ì‚¬ì¹™ ì—°ì‚° ì—°ìŠµ</title>
 </head>
 <body>
-<h1>»çÄ¢ ¿¬»ê ¿¬½À</h1> 
+<h1>ì‚¬ì¹™ ì—°ì‚° ì—°ìŠµ</h1> 
 <form action="<?=$_SERVER['PHP_SELF']?>" method=POST>
 <input type="text" name="n1" value="<?php echo $_POST['n1']; ?>">
 +
@@ -16,16 +16,16 @@
 =
 <input type="text" name="0" value="<?php echo $_POST['n3']-$_POST['n4']; ?>"><br>
 <input type="text" name="n5" value="<?php echo $_POST['n5']; ?>">
-¡¿ 
+Ã— 
 <input type="text" name="n6" value="<?php echo $_POST['n6']; ?>">
 =
 <input type="text" name="0" value="<?php echo $_POST['n5']*$_POST['n6']; ?>"><br>
 <input type="text" name="n7" value="<?php echo $_POST['n7']; ?>">
-¡À
+Ã·
 <input type="text" name="n8" value="<?php echo $_POST['n8']; ?>">
 = 
 <input type="text" name="0" value="<?php if($_POST['n8'] != 0){echo $_POST['n7']/$_POST['n8'];} ?>"><br>
-<input type="submit" value="Àü¼Û">
+<input type="submit" value="ì „ì†¡">
 </form>
 </body>
 </html>
